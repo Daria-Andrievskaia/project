9baf759fd9b9eb1b26dddf8db9c96be6e1454452
Dasha <andrievskaya.dd@gmail.com>
2021-01-05 11:57:02 +0100

    starting to create an application

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-"use strict";
-const answers = [];
-answers[0] = prompt ("Как ваше имя?", "");
-answers[1] = prompt ("Как ваше фамилия?", "");
-answers[2] = prompt ("Сколько вам лет?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-document.write(answers);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
